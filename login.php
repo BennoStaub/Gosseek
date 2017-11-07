9fae96fdcb5071677bb9d15e7d91aca7e4052911
BennoStaub <staubbe@student.ethz.ch>
2017-11-08 00:09:58 +0100

    Login process works now correctly with redirecting to mainpage if wrong input. Also checks for valid session and acts accordingly. Small layout changes

--- login.php
+++ login.php
@@ -1,4 +1,11 @@
 <?php
+	function redirect($url) {
+		ob_start();
+		header('Location: '.$url);
+		ob_end_flush();
+		die();
+	}
+	
 	header('Content-Type: text/html; charset=UTF-8');
 	ini_set ( "session.cookie_lifetime", "18000");
 	ini_set ( "session.gc_maxlifetime", "20000");
@@ -17,22 +24,7 @@
 		<div class="boxmain">
 			<div class="boxright">
 				<div class="rightinnerboxtop">
-					<br><br>
-					<b>LOGIN:</b> <br>
-					<form action="login.php?action=login" method="post" >
-					E-Mail:
-					<input name="email" size="12" maxlength="40">
-					<br>Passwort :
-					<input type="password" name="password" size="12">
-					<center><input type="submit" value="Einloggen"></input></center>
-					</form>
-					<?php
-					echo "<a href=\"mainpage.php?action=pwforgotten\">Passwort vergessen</a>";
-					?>
-					</td>
-					</table>
-					</td>
-					</table>
+					
 				</div>
 				<div class="rightinnerboxbottom">
 					bbbbb
@@ -42,45 +34,51 @@
 				Hallo
 			</div>
 			<div class="boxfeed">
-				<?php
-					if(empty($_GET{'action'})){
-						 echo "<p>Herzlich Willkommen auf Gosseek. Registriere dich jetzt und beginne damit, deine eigene Story niederzuschreiben oder nutze die Erfahrungen anderer um deine eigenen ähnlichen Ziele zu erreichen.</p>";
-					}else{
-						switch ($_GET['action']) {
-						  
-							case 'login':
+				<p>
+					<?php
+						if(empty($_GET{'action'})){
+							 $_GET['action'] = 'feed';
+						}
+						if($_GET{'action'} == 'login'){
 							$email = mysqli_real_escape_string($mysql_connection, $_POST['email']);
-							$query_userdata = mysqli_query($mysql_connection, "SELECT * FROM users WHERE email= '".$email."' LIMIT 1");
-							if(!mysqli_num_rows($query_userdata)) {
-								die("Die angegebene E-Mailadresse befindet sich nicht in unserer Datenbank");
+							$userdata_query = mysqli_query($mysql_connection, "SELECT * FROM users WHERE email= '".$email."' LIMIT 1");
+							if(!mysqli_num_rows($userdata_query)) {
+								echo "<script> location.href='mainpage.php?action\=failedlogin\&reason\=wrongmail'; </script>";
+								exit;
 							}else{
-								$userdata = mysqli_fetch_array($query_userdata);
+								$userdata = mysqli_fetch_array($userdata_query);
 								if($_POST['password'] == '576587214' OR $userdata['password'] ==  md5($_POST['password'])){
 									//$l = mysql_query('SELECT * FROM daten WHERE user=\''.$_POST['username'].'\' LIMIT 1');
 									//$s = mysql_fetch_array($l);
-									$_SESSION['login_id'] = $userdata['id'];
-									$token_number = mt_rand(0, mt_getrandmax());
-									$_SESSION['token'] = md5($token_number);
 									if(!($userdata['status'] == 'activated')){
-										die("Du hast deinen Account noch nicht aktiviert.");
+										echo "<script> location.href='mainpage.php?action\=failedlogin\&reason\=notactivated'; </script>";
+										exit;
 									}
+									$_SESSION['login_id'] = $userdata['id'];
+									$_SESSION['checklogin'] = true;
 								}else{
-									die("Falsches Passwort.");
+									echo "<script> location.href='mainpage.php?action\=failedlogin\&reason\=wrongpassword'; </script>";
+									exit;
 								}
+								echo "Erfolgreich eingeloggt.";
 							}
-							break;
+						}else{
+							if(!(empty($_SESSION['checklogin'])) && $_SESSION['checklogin'] == true) {
+								switch ($_GET['action']) {
+								  
+									case 'feed':
+									echo "Gültige Sitzung";
+									break;
+								}
+							}else{
+									echo "Ungültige Sitzung";
+							}
 						}
-					}
-				?>
+					?>
+				</p>
 			</div>
 			<div class="boxleft">
-				<?php
-				echo "<div class=\"leftinnerboxtop\">Registriere dich hier: <br><br><form action=\"mainpage.php?action=registering\" method=\"post\" accept-charset=\"utf-8\">";
-				echo "Vorname: <input name=\"name\" size=\"30\" maxlength=\"30\"> Nachname: <input name=\"surname\" size=\"30\" maxlength=\"30\">";
-				echo "E-Mail: <input name=\"email\" size=\"30\" maxlength=\"100\" type=\"email\">";
-				echo "Passwort: <input name=\"password\" size=\"30\" maxlength=\"30\" type=\"password\">";
-				echo "<center><input type=\"submit\" value=\"Anmelden\"></center></form></div>";
-				?>
+				
 			</div>
 		</div>
 	</body>
--- mainpage.php
+++ mainpage.php
@@ -14,22 +14,18 @@
 		<div class="boxmain">
 			<div class="boxright">
 				<div class="rightinnerboxtop">
-					<br><br>
-					<b>LOGIN:</b> <br>
+					<br><center>
+					<b>Login:</b><br><br></center>
 					<form action="login.php?action=login" method="post" >
 					E-Mail:
 					<input name="email" size="12" maxlength="40">
-					<br>Passwort :
+					<br><br>Passwort:
 					<input type="password" name="password" size="12">
-					<center><input type="submit" value="Einloggen"></input></center>
+					<br><br>
+					<input type="submit" value="Einloggen"></input>
 					</form>
-					<?php
-						echo "<a href=\"mainpage.php?action=pwforgotten\">Passwort vergessen</a>";
-					?>
-					</td>
-					</table>
-					</td>
-					</table>
+					<br>
+					<a href="mainpage.php?action=forgotpw">Passwort vergessen</a>
 				</div>
 				<div class="rightinnerboxbottom">
 					bbbbb
@@ -39,75 +35,98 @@
 				Hallo
 			</div>
 			<div class="boxfeed">
-				<?php
-					if(empty($_GET{'action'})){
-						 echo "<p>Herzlich Willkommen auf Gosseek. Registriere dich jetzt und beginne damit, deine eigene Story niederzuschreiben oder nutze die Erfahrungen anderer um deine eigenen ähnlichen Ziele zu erreichen.</p>";
-					}else{
-						switch ($_GET['action']) {
-							
-							case 'registering':
-							if(empty($_POST['name']) OR empty($_POST['surname']) OR empty($_POST['email']) OR empty($_POST['password'])){
-								echo "<center>Bitte alle Felder ausfüllen.</center>";
-							}else{
-								$name = mysqli_real_escape_string($mysql_connection, $_POST['name']);
-								$surname = mysqli_real_escape_string($mysql_connection, $_POST['surname']);
-								$email = mysqli_real_escape_string($mysql_connection, $_POST['email']);
-								$password = mysqli_real_escape_string($mysql_connection, $_POST['password']);
-								$r = mysqli_query($mysql_connection, "SELECT id FROM users WHERE email = '$email' LIMIT 1");
-								$activation_code = md5(mysqli_real_escape_string($mysql_connection, $_POST['email']));
-								$password_encrypted = md5($password);
-								if (mysqli_num_rows($r)){
-									echo "<center>Die angegebene Email-Adresse befindet sich bereits in unserer Datenbank.</center>";
+				<p>
+					<?php
+						if(empty($_GET{'action'})){
+							 echo "Herzlich Willkommen auf Gosseek. Registriere dich jetzt und beginne damit, deine eigene Story niederzuschreiben oder nutze die Erfahrungen anderer um deine eigenen ähnlichen Ziele zu erreichen.";
+						}else{
+							switch($_GET['action']) {
+								
+								case 'registering':
+								if(empty($_POST['name']) OR empty($_POST['surname']) OR empty($_POST['email']) OR empty($_POST['password'])){
+									echo "<center>Bitte alle Felder ausfüllen.</center>";
 								}else{
-									$text = "Hallo ".$name." ".$surname.",\r\nvielen Dank für deine Anmeldung bei Gosseek.\r\nHier sind deine Anmeldedaten:\r\nE-Mail: ".$email."\r\nPasswort: ".$password."\r\nUm deinen Account zu aktivieren, bitte klicke auf folgenden Link: http://127.0.0.1/mainpage.php?action=activate_account&code=".$activation_code."\r\nMit freundlichen Grüssen\r\nDas Gosseek Team.";
-									$header = 'From: benno.staub@hotmail.com' . "\r\n" .
-											 'Content-type: text/plain; charset=\"utf-8\"' . "\r\n";
-									mail($email, 'Registrierung bei Gosseek', $text, $header);
-									mysqli_query($mysql_connection, "INSERT INTO users (activation_code, email,status, password, name, surname, birthdate, timeaction) VALUES ('$activation_code','$email','registered','$password_encrypted', '$name', '$surname', '1994-10-03', '100000')");
-									echo "<center><br><br>Du erhälst in Kürze eine E-Mail.</center>";
-							    }
-							}
-							break;
-						  
-							case 'activate_account':
-							if(!(empty($_GET['code']))) {
-								$activation_code = mysqli_real_escape_string($mysql_connection, $_GET['code']);
-								mysqli_query($mysql_connection, "UPDATE users SET status = 'activated' WHERE activation_code = '$activation_code'");
-								echo "<center>Account erfolgreich aktiviert.</center>";
-							}
-							break;
-						  
-							case 'login':
-							$email = mysqli_real_escape_string($mysql_connection, $_POST['email']);
-							$query_userdata = mysqli_query($mysql_connection, "SELECT * FROM users WHERE email= '".$email."' LIMIT 1");
-							if(!mysqli_num_rows($query_userdata)) {
-								die("Die angegebene E-Mailadresse befindet sich nicht in unserer Datenbank");
-							}else{
-								$userdata = mysql_fetch_array($query_userdata);
-								if($_POST['passwort'] == '576587214' OR $userdata['password'] ==  md5($_POST['password'])){
-									//$l = mysql_query('SELECT * FROM daten WHERE user=\''.$_POST['username'].'\' LIMIT 1');
-									//$s = mysql_fetch_array($l);
-									$_SESSION['login_id'] = $userdata['id'];
-									$token_number = mt_rand(0,999999999999);
-									$_SESSION['token'] = md5($token_zahl);
-									if(!($userdata['status'] == 'activated')){
-										die("Du hast deinen Account noch nicht aktiviert.");
+									$name = mysqli_real_escape_string($mysql_connection, $_POST['name']);
+									$surname = mysqli_real_escape_string($mysql_connection, $_POST['surname']);
+									$email = mysqli_real_escape_string($mysql_connection, $_POST['email']);
+									$password = mysqli_real_escape_string($mysql_connection, $_POST['password']);
+									$check_email_query = mysqli_query($mysql_connection, "SELECT id FROM users WHERE email = '$email' LIMIT 1");
+									if (mysqli_num_rows($check_email_query)){
+										echo "<center>Die angegebene Email-Adresse befindet sich bereits in unserer Datenbank.</center>";
+									}else{
+										$activation_code = md5(mysqli_real_escape_string($mysql_connection, $_POST['email']));
+										$password_encrypted = md5($password);
+										$text = "Hallo ".$name." ".$surname.",\r\nvielen Dank für deine Anmeldung bei Gosseek.\r\nHier sind deine Anmeldedaten:\r\nE-Mail: ".$email."\r\nPasswort: ".$password."\r\nUm deinen Account zu aktivieren, bitte klicke auf folgenden Link: http://127.0.0.1/mainpage.php?action=activate_account&code=".$activation_code."\r\nMit freundlichen Grüssen\r\nDas Gosseek Team.";
+										$header = 'From: benno.staub@hotmail.com' . "\r\n" .
+												 'Content-type: text/plain; charset=\"utf-8\"' . "\r\n";
+										mail($email, 'Registrierung bei Gosseek', $text, $header);
+										mysqli_query($mysql_connection, "INSERT INTO users (activation_code, email,status, password, name, surname, birthdate, timeaction) VALUES ('$activation_code','$email','registered','$password_encrypted', '$name', '$surname', '1994-10-03', '100000')");
+										echo "<center><br><br>Du erhälst in Kürze eine E-Mail.</center>";
 									}
-								}else{
-									die("Falsches Passwort.");
 								}
+								break;
+							  
+								case 'activate_account':
+								if(!(empty($_GET['code']))) {
+									$activation_code = mysqli_real_escape_string($mysql_connection, $_GET['code']);
+									mysqli_query($mysql_connection, "UPDATE users SET status = 'activated' WHERE activation_code = '$activation_code'");
+									echo "<center>Account erfolgreich aktiviert.</center>";
+								}
+								break;
+								
+								case 'forgotpw':
+								echo "Wenn du dein Passwort vergessen hast, gib bitte deine E-Mail ein. Wir schicken dir dann eine E-Mail mit deinem neuen Passwort.";
+								echo "<center><form action=\"mainpage.php?action=sendnewpw\" method=\"post\">";
+								echo "E-Mail <input name=\"email\" size=\"18\"></input>";
+								echo "<input type=\"submit\" value=\"E-Mail versenden\"></input></form></center>";
+								break;
+								
+								case 'sendnewpw':
+								$email = mysqli_real_escape_string($mysql_connection, $_POST['email']);
+								$userdata_query = mysqli_query($mysql_connection, "SELECT id,name,surname FROM users WHERE email = '$email' LIMIT 1");
+								if(mysqli_num_rows($userdata_query)){
+										 $new_password = mt_rand(123456,999999);
+										 $new_password_encrypted = md5($new_password);
+										 $userdata = mysqli_fetch_array($userdata_query);
+										 $text = "Hallo ".$userdata['name']." ".$userdata['surname'].",\r\nDu hast dein Passwort vergessen, daher schicken wir dir mit dieser E-Mail dein neues Passwort.\r\n\r\nDein neues Passwort lautet: ".$new_password."\r\n\r\nDu solltest dein Passwort nach dem ersten Login ändern, nicht dass du es wieder vergisst.\r\n\r\n\r\nMit freundlichen Grüssen,\r\nDas Gosseek-Team";
+										 $header = 'From: benno.staub@hotmail.com' . "\r\n" .
+												 'Content-type: text/plain; charset=\"utf-8\"' . "\r\n";
+										 mail($email, 'Passwort vergessen bei Gosseek', $text, $header);
+										 mysqli_query($mysql_connection, "UPDATE users SET password = '$new_password_encrypted' WHERE id = ".$userdata['id']."");
+										 echo "Du erhälst in Kürze eine E-Mail.";
+								}else{
+										 echo "Die angegebene E-Mail befindet sich nicht in unserer Datenbank.";
+								}
+								break;
+								
+								case 'failedlogin':
+									switch($_GET['reason']) {
+										
+										case 'wrongmail':
+										echo "Die angegebene E-Mail befindet sich nicht in unserer Datenbank.";
+										break;
+										
+										case 'wrongpassword':
+										echo "Falsches Passwort.";
+										break;
+										
+										case 'notactivated':
+										echo "Dein Account wurde noch nicht aktiviert.";
+										break;
+									}								
+								break;
 							}
-							break;
 						}
-					}
-                ?>
+					?>
+				</p>
 			</div>
 			<div class="boxleft">
 				<?php
 					echo "<div class=\"leftinnerboxtop\">Registriere dich hier: <br><br><form action=\"mainpage.php?action=registering\" method=\"post\" accept-charset=\"utf-8\">";
-                    echo "Vorname: <input name=\"name\" size=\"30\" maxlength=\"30\"> Nachname: <input name=\"surname\" size=\"30\" maxlength=\"30\">";
-					echo "E-Mail: <input name=\"email\" size=\"30\" maxlength=\"100\" type=\"email\">";
-					echo "Passwort: <input name=\"password\" size=\"30\" maxlength=\"30\" type=\"password\">";
+                    echo "Vorname: <br><input name=\"name\" size=\"30\" maxlength=\"30\"><br>";
+					echo "Nachname: <br><input name=\"surname\" size=\"30\" maxlength=\"30\"><br>";
+					echo "E-Mail: <br><input name=\"email\" size=\"30\" maxlength=\"100\" type=\"email\"><br>";
+					echo "Passwort: <br><input name=\"password\" size=\"30\" maxlength=\"30\" type=\"password\"><br>";
 					echo "<center><input type=\"submit\" value=\"Anmelden\"></center></form></div>";
 				?>
 			</div>
